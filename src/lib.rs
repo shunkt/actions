8108a704c558a470f927de98d0c9164b3105d3d6
Shunsuke Kawabata <shun.92@gmail.com>
2022-06-12 02:00:14 +0900

    add: actions

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
+pub struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    pub fn new(x: isize, y: isize) -> Self {
+        Self { x, y }
+    }
+
+    pub fn square(&self) -> isize {
+        self.x * self.y
+    }
+}
 #[cfg(test)]
 mod tests {
+    use super::Point;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_squre() {
+        let p = Point::new(2, 2);
+        assert_eq!(p.square(), 4);
     }
 }
